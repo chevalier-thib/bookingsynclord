a62b57f12b1c0e48986f2f4a7be3a9aa1de46b95
Ubuntu <ubuntu@dev.bnbstaff.net>
2016-04-17 11:28:15 +0000

    fix import not working in disted version

--- bookingsynclord/BookingSyncAPI.py
+++ bookingsynclord/BookingSyncAPI.py
@@ -1,6 +1,6 @@
 import config
 import logging
-import tools
+from tools.CrendentialManager import CredentialManger
 from data_store.RentalStore import RentalStore
 from data_store.BookingStore import BookingStore
 
@@ -8,6 +8,6 @@ logger = logging.getLogger(__name__)
 
 class BookingSyncAPI:
     def __init__(self,client_id,client_secret,access_token,refresh_token=None):
-        self.credential_manager = tools.CredentialManager.CredentialManager(client_id,client_secret,access_token,refresh_token)
+        self.credential_manager = CredentialManager(client_id,client_secret,access_token,refresh_token)
         self.rentals_store = RentalStore(self.credential_manager)
         self.booking_store = BookingStore(self.credential_manager)
