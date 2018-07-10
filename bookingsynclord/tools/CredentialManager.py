8754065e636d282dd5952ce5ba6cb4adf02c4445
neartik <neartik@gmail.com>
2018-07-10 13:07:16 +0200

    Quentin (#4)
    * If no refresh token in bookingsync answer, return the previous one
    
    * Add specific exception

--- bookingsynclord/tools/CredentialManager.py
+++ bookingsynclord/tools/CredentialManager.py
@@ -1,5 +1,18 @@
 import requests
 from bookingsynclord import config
+
+
+class AuthException(Exception):
+    def __init__(self, data):
+        # Invoke the super class's __init__
+        super(AuthException, self).__init__(data)
+        try:
+            self.error = data['error']
+            self.error_description = data['error_description']
+        except Exception:
+            self.data = data
+
+
 class CredentialManager:
     """Maintain oAuth Crendential.
     used to refresh token, generate authorization URL
@@ -10,7 +23,7 @@ class CredentialManager:
         self.access_token = access_token
         self.refresh_token = refresh_token
 
-    def use_refresh_token(self,refresh_token):
+    def use_refresh_token(self, refresh_token):
         """Send a POST request using the refresh_token and
         generate a new access token.
         Set the current access_token to the new token
@@ -25,12 +38,14 @@ class CredentialManager:
         refresh_param="client_id={}&client_secret={}&refresh_token={}&expires_in=1234&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob".format(self.client_id,self.client_secret, refresh_token)
         r = requests.post(url_request,params=refresh_param,timeout=config.REQUEST_DEFAULT_TIMEOUT)
         json_response = r.json()
-        refresh_token = json_response["refresh_token"]
-        access_token = json_response["access_token"]
-        self.access_token = access_token
-        self.refresh_token = refresh_token
-        return access_token,refresh_token
-
+        try:
+            refresh_token = json_response["refresh_token"]
+            access_token = json_response["access_token"]
+            self.access_token = access_token
+            self.refresh_token = refresh_token
+            return access_token, refresh_token
+        except Exception:
+            raise AuthException(json_response)
 
     def roll_token(self):
         """Call refresh_token method with currently
